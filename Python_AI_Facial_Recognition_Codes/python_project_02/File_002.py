fcdcc0a5e9a7ffae29170bdc48229438e41c5ce4
JvTU_Axiomer <2748823232@qq.com>
2023-05-23 20:52:35 +0800

    Python_Class_Code_2023-5-23.

---
+++ Python_AI_Facial_Recognition_Codes/python_project_02/File_002.py
@@ -0,0 +1,28 @@
+# 人生哪能多如意，万事只求半称心.
+# @Time : 2023/5/23 20:21
+# @Author : JvTU_Axiomer
+# @Version : 
+# @IDE : PyCharm
+# @Project : python_project_02
+
+# <class 'int'> —— 整型.
+a = 100
+print(type(a))
+
+# <class 'float'> —— 浮点类型.
+b = 3.14
+print(type((b)))
+
+# 明确声明了当前变量的类型.
+c: int = 200
+
+# <class 'bool'> —— 布尔类型
+d = True
+f = False
+print(type(d))
+print(type(f))
+
+# <class 'str'> —— 字符转类型
+g = 'abc123'
+h = '中国人'
+print(type(g))
